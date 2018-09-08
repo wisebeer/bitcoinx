034b489d43d1c2b4e31c4f2751ce9630bd7ed698
wisebeer <wisebeer@qq.com>
2018-09-08 17:43:43 +0800

    完善编译指令

--- src/bhashmain.cpp
+++ src/bhashmain.cpp
@@ -1,12 +1,47 @@
 /*
- compile: g++ -I./ -I./contract/evm/ bhashmain.cpp -o bhashmain
+ compile: 
+ g++ bhashmain.cpp -o bhashmain -I. -I./contract/evm -L. -lbitcoin_consensus -lbitcoin_util -L./crypto -lbitcoin_crypto -L/usr/lib/x86_64-linux-gnu -lboost_system -lpthread -lcrypto
 */
 
-
 #include "primitives/block.h"
+#include "hash_blake2.h"
+
+void test();
 
 int main(int argc, char *argv[])
 {
     std::cout << "hi!!!" << std::endl;
+
+    test();
+
     return 0;
+}
+
+uint32_t getNbits(const std::string& str);
+
+uint256 toUint256(const std::string& str);
+
+void test()
+{
+    // block 674344
+	// https://bcx.info/block/00000000000003310cb642a1b2f8819673d56df0e7ab0818a5ecbb3a80f69a17
+    CBlockHeader header;
+    header.nVersion = 838860800;
+    header.hashPrevBlock = toUint256("0000000000001934b1ba1864359d8dc51af7c67efadcd78341cb97053f6d9349");
+    header.hashMerkleRoot = toUint256("1b931ec4d4ef13f2e1d31d345c5ebea67e41468662c10354fae9a4f5073972c9");
+    header.nTime = 1536240953;
+    header.nBits = 0;
+    header.nNonce = 13972797;
+
+    uint256 result = header.GetHash();
+}
+
+uint32_t getNbits(const std::string& str)
+{
+    return 0;
+}
+
+uint256 toUint256(const std::string& str)
+{
+    return uint256();
 }
