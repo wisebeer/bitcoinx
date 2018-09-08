3dc178f308bb16d5e8bc635350ea8264ddf7d159
wisebeer <wisebeer@qq.com>
2018-09-08 11:36:16 +0800

    添加bhashmain.cpp

---
+++ src/bhashmain.cpp
@@ -0,0 +1,12 @@
+/*
+ compile: g++ -I./ -I./contract/evm/ bhashmain.cpp -o bhashmain
+*/
+
+
+#include "primitives/block.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hi!!!" << std::endl;
+    return 0;
+}
